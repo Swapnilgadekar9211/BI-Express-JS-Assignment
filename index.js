cae272970da146263c4998517b6b15428647c52f
Swapnil Gadekar <101463184+Swapnilgadekar9211@users.noreply.github.com>
2022-09-18 17:14:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const express = require('express');
+
+const app = express();
+const bp = require('body-parser');
+app.use(bp.json());
+const port = process.env.PORT || 5000;
+
+const addDetails = require('./Problem1/StudentDetails');
+app.use('/student', addDetails);
+const getDetails = require('./Problem1/getStudentDetails');
+app.use('/student', getDetails);
+
+const studList = require('./Problem2/liststudent')
+app.use('/student', studList)
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
